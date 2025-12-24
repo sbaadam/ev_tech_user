22b3d0b4f94fbc4d9187aa8087b7e2c5df6ab09b
sbaadam <s.baadam1601@gmail.com>
2025-12-24 15:37:48 +0530

    - init project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.example.ev_tech_user"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
